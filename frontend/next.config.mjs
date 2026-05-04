b8ea0e14dccfb9c4c17560c52a8abd2aa1963d85
yaysin <ykarakartal>
2026-05-04 22:27:22 +0200

    fixed next.config.mjs/frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        allowedDevOrigins: [
+            "http://backend.book-review.svc.cluster.local:3001",
+            "http://backend:3001",
+            "http://localhost:3001"
+        ]
+    }
+};
 
 export default nextConfig;
